1b4ee54fb10b8cde5068c302d6928208fd51db8b
Michael Verdegaal <michaelverdegaal@hotmail.nl>
2020-12-17 22:06:22 +0100

    Switch to flask and leaflet

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 ###
 # This file will change frequently until all the modules have been written, for testing purposes
 ###
-from data.dashboard import create_worldmap, update_worldmap_settings, create_dash_app
+from config import MBTOKEN
 from data.database import *
 from data.dataframes import create_marker_label_data
+from flask import Flask, render_template, abort
+from jinja2 import TemplateNotFound
 
 # Data
 db = create_connection()
-df = query_hotels(db)
+all_hotels = query_hotels(db)
 
-# Worldmap
-customdata = create_marker_label_data(df)
-fig = create_worldmap(df['lat'], df['lng'], customdata)
-update_worldmap_settings(fig)
+# Map data
+customdata = create_marker_label_data(all_hotels)
 
-# Dash app
-app = create_dash_app(fig)
-app.run_server(debug=True)
+# Flask
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
+
+@app.route('/')
+def homepage():
+    try:
+        return render_template('homepage.html', token=MBTOKEN)
+    except TemplateNotFound:
+        abort(404)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=656565)
